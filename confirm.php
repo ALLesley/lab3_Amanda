beeea8a59c82ba6794014680d4937a2bc5117c28
Amanda Lesley <misspanda8000@gmail.com>
2026-01-29 15:43:05 -0600

    add files

---
+++ confirm.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Hello <?php echo $_POST["name"]; ?> <br>
+Email: <?php echo $_POST["email"]; ?> <br>
+Age: <?php echo $_POST["name"]; ?> <br>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="confirm.php" method="POST">
+Name: <input type="text" name="name"> <br>
+Email: <input type="text" name="email"> <br>
+Age: <input type="text" name="age"> <br>
+<input type="submit">
+
+</form>
+
+</body>
+
+</html>
